554430fccf4893f7802f0e53e245324435120878
Wang Zixu <wangzixu530@gmail.com>
2024-05-09 23:54:10 +0800

    Initial project

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+}
